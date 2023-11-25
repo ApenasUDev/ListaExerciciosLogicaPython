6efb2d27d0065a9451e92e3b2c293e390c0975f7
ApenasUDev <francimaralexandredt@gmail.com>
2023-11-25 15:50:25 -0300

    .

---
+++ EstruturaSequencial/exercicio003.py
@@ -0,0 +1,6 @@
+"""
+Faça um Programa que peça dois números e imprima a soma.
+"""
+numero1 = int(input("digite um número: "))
+numero2 = int(input("digite outro número: "))
+print(f"a soma dois dois número é: {numero1+numero2}")
---
+++ EstruturaSequencial/exercicio004.py
@@ -0,0 +1,8 @@
+"""
+Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""
+nota1 = float(input("nota 1: "))
+nota2 = float(input("nota 2: "))
+nota3 = float(input("nota 3: "))
+nota4 = float(input("nota 4: "))
+print(f"sua média: {(nota1+nota2+nota3+nota4)/4:.1f}")
