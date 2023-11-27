f59e36c7d6f301d4515cda88f2a0e36228c06be8
ApenasUDev <francimaralexandredt@gmail.com>
2023-11-27 07:16:47 -0300

    novos exercícios

---
+++ EstruturaSequencial/exercicio005.py
@@ -0,0 +1,6 @@
+"""
+Faça um Programa que converta metros para centímetros. 
+"""
+m = float(input("metros: "))
+cm = m*100
+print(f"{m}(m) convertidos para centímetros fica {cm}cm")
---
+++ EstruturaSequencial/exercicio006.py
@@ -0,0 +1,7 @@
+"""
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
+"""
+raio = float(input("raio: "))
+pi = 3.1416
+area = pi*(raio**2)
+print(f"a área do círculo é {area:.2f}")
